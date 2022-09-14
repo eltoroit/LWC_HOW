8b51143ec37aef3656fe0fb0d1b0120d705ddd01
Andres Perez <aperez@salesforce.com>
2022-09-14 07:28:43 -0400

    Tree Grid

--- force-app/main/default/lwc/redAccountsData/redAccountsData.js
+++ force-app/main/default/lwc/redAccountsData/redAccountsData.js
@@ -8,12 +8,34 @@ export default class RedAccountsData extends LightningElement {
     columns = [
         { label: 'Account Name', fieldName: 'Name' },
         { label: 'Last Activity', fieldName: 'LastActivityDate' },
+        { label: 'Contact Name', fieldName: 'ContactName' },
+        { label: 'Email', fieldName: 'Email', type: "email" },
+        { label: 'Phone', fieldName: 'Phone', type: "phone" },
     ]
 
     @wire(getRedAccounts, {maxRecords: '$maxRecords'})
     wired_GetRedAccounts({ data, errors }) {
         if (data) {
-            this.records = data;
+            this.records = data.map(account => {
+                let newAccount = {
+                    Id: account.Id,
+                    Name: account.Name,
+                    LastActivityDate: account.LastActivityDate,
+                    _children : []
+                }
+                if (account.Contacts?.length > 0) {
+                    newAccount._children = account.Contacts.map(contact => {
+                        let newContact = {
+                            Id: contact.Id,
+                            ContactName: contact.Name,
+                            Email: contact.Email,
+                            Phone: contact.Phone
+                        }
+                        return newContact;
+                    })
+                }
+                return newAccount;
+            });
         } else if (errors) {
             this.records = [];
         }
